e18ddd412dd4e1497af2240ea03a8d8dcc8a785e
vijayavaddi <vijayavaddi9494@gmail.com>
2025-06-07 20:02:46 +0530

    Delete src/main/java/com/hcltech/doctorPatientService directory
    deleted duplication

--- src/main/java/com/hcltech/doctorPatientService/repository/SpecializationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hcltech.doctorPatientService.repository;
-
-import com.hcltech.doctorPatientService.model.Specialization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpecializationRepository  extends JpaRepository<Specialization,Integer> {
-
-}
